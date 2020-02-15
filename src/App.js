1ff735ea886576b9be31627dcec3db0b9efecafe
Alessio <alessioferriweb@gmail.com>
2020-02-15 23:39:54 +0000

    Using first hook useState

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [name, setName] = useState("");
+    return (
+        <div className="App">
+            <header className="App-header">
+                <p>
+                    Type your name below
+                </p>
+                <input type="text" onChange={(event)=>setName(event.target.value)} name="name"/>
+                <p>
+                    Your name is {name}
+                </p>
+            </header>
+        </div>
+    );
 }
 
 export default App;
